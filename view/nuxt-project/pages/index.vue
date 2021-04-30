ce48fe00033fe54f56044bb72164853925a8a523
oba18 <m.oba.0426@gmail.com>
2021-05-01 00:11:58 +0900

    (#3)

---
+++ view/nuxt-project/pages/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <b-row>
+      <b-col cols=3></b-col>
+      <b-col>
+        <b-row>
+          <b-col>
+            <h1>Log in</h1>
+          </b-col>
+        </b-row>
+        <br>
+        <b-row>
+          <b-col>
+            <b-form-input
+              v-model="email"
+              type="email"
+              placeholder="Enter Email"
+              required
+              ></b-form-input>
+          </b-col>
+        </b-row>
+        <br>
+        <b-row>
+          <b-col>
+            <b-form-input
+              v-model="password"
+              type="password"
+              placeholder="Enter Password"
+              required
+              ></b-form-input>
+          </b-col>
+        </b-row>
+        <br>
+        <b-row>
+          <b-col>
+            <b-button @click="loginWithAuthModule">log in</b-button>
+          </b-col>
+        </b-row>
+      </b-col>
+      <b-col cols=3></b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async loginWithAuthModule() {
+      await this.$auth.loginWith("local", {
+          data: {
+            email: this.email,
+            password: this.password
+          }
+        })
+        .then(
+          response => {
+            localStorage.setItem("access-token", response.headers["access-token"]);
+            localStorage.setItem("client", response.headers.client);
+            localStorage.setItem("uid", response.headers.uid);
+            localStorage.setItem("token-type", response.headers["token-type"]);
+            return response;
+          },
+          error => {
+            return error;
+          }
+        );
+    }
+  }
+}
+</script>
+
---
+++ view/nuxt-project/pages/members.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-row>
+    <b-col cols=3></b-col>
+    <b-col>
+      <h1>members</h1>
+      <hr>
+      <div v-for="user in users" :key=user.id>
+        <li style="font-size:22px">
+          <b-badge variant="primary" v-if="user.role_id == 1">講師</b-badge>
+          <b-badge variant="primary" v-if="user.role_id == 2">生徒</b-badge>
+          <b-link>{{ user.name }}</b-link>
+        </li>
+      </div>
+    </b-col>
+    <b-col cols=3></b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    this.$axios.get('/api/v1/users', {
+      headers: { 
+        "Content-Type": "application/json", 
+      },
+    }).then(response => {
+        this.users = response.data
+      })
+  },
+}
+</script>
