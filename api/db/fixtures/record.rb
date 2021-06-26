4e85af342a685555325e0991f4fc3d99c8fe7325
Kugue99A <hisabisa99yohey@gmail.com>
2021-06-26 11:03:09 +0900

    [mod] record fixtures (#31)

--- api/db/fixtures/record.rb
+++ api/db/fixtures/record.rb
@@ -1,3 +1,4 @@
 Record.seed( :id,
-  { id: 1, title: 'test_curriculum', content: 'test_content', homework: 'test_homework', user_id: 1, curriculum_id: 1 },
+  { id: 1, title: 'install vue', content: 'test_content', homework: 'test_homework', user_id: 1, curriculum_id: 1 },
+  { id: 1, title: 'write vue code', content: 'test_content', homework: 'test_homework', user_id: 1, curriculum_id: 1 },
 )
